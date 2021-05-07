105fa332cd74ca5d976236235c1158e57c1aab0b
gentleman9999 <gentleman99729@outlook.com>
2021-05-07 09:00:45 +0700

    Fix type warning

--- src/components/Dashboard/ClientList/index.tsx
+++ src/components/Dashboard/ClientList/index.tsx
@@ -243,7 +243,7 @@ export default function ClientList() {
                         onChange={(e) => {
                           toggleActive(e, item.id);
                         }}
-                        name={item.id}
+                        name={item.id.toString()}
                       />
                       <IconButton
                         onClick={() => {
