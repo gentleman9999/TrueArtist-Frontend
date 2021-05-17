2243e80e792d1bbbde9931c8a856605ce0466923
gentleman9999 <gentleman99729@outlook.com>
2021-05-17 09:31:22 +0700

    Add google autocomplete to city fields

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,5 +1,5 @@
 // External
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import clsx from "clsx";
 
 import TextField, { TextFieldProps } from "@material-ui/core/TextField";
@@ -11,7 +11,12 @@ import Visibility from "@material-ui/icons/Visibility";
 import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import FormControl from "@material-ui/core/FormControl";
 
-type CustomTextFieldProps = TextFieldProps & { control: any; errors: any };
+type CustomTextFieldProps = TextFieldProps & {
+  control: any;
+  errors: any;
+  setValueFn?: any;
+  googleAutoComplete?: boolean;
+};
 
 export default function FormInput(props: CustomTextFieldProps) {
   const customProps = { ...props };
@@ -19,12 +24,50 @@ export default function FormInput(props: CustomTextFieldProps) {
   delete customProps.control;
   delete customProps.errors;
   delete customProps.defaultValue;
+  delete customProps.setValueFn;
+  delete customProps.googleAutoComplete;
 
   const [showPassword, setShowPassword] = useState(false);
 
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword);
   };
+
+  useEffect(() => {
+    if (props.googleAutoComplete) {
+      // Create the search box and link it to the UI element.
+      const input = document.getElementById(props.id as string) as HTMLInputElement;
+      const searchBox = new google.maps.places.SearchBox(input);
+
+      // Set place value
+      const setPlaceValue = () => {
+        const places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+          return;
+        }
+
+        props.setValueFn(props.name, places[0].name);
+      };
+
+      searchBox.addListener("places_changed", setPlaceValue);
+
+      if (!props.defaultValue) {
+        setTimeout(() => {
+          // Remove input focus class, By default, adding google search to input field will active focus state of input
+          const label = document.querySelectorAll(`.form-control-${props.id} label`);
+          label[0].classList.remove("MuiInputLabel-shrink");
+
+          const input = document.querySelectorAll(`.form-control-${props.id} input`);
+          input[0].classList.remove("pac-target-input");
+        }, 100);
+      }
+
+      return () => searchBox.unbindAll();
+    } else {
+      return () => {};
+    }
+  }, []);
 
   return (
     <Controller
@@ -37,7 +80,11 @@ export default function FormInput(props: CustomTextFieldProps) {
             {customProps.type !== "password" && (
               <TextField
                 {...customProps}
-                className={clsx(props.className, `${props.errors && props.errors.message ? "error" : ""}`)}
+                className={clsx(
+                  props.className,
+                  `form-control-${props.id}`,
+                  `${props.errors && props.errors.message ? "error" : ""}`,
+                )}
                 value={value}
                 onChange={(e) => {
                   onChange(e.target.value);
